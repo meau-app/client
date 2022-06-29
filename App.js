2ca28fb5bd2ab80e782b232f1b8e1b9bafb04e1b
bruno <bruno@vsaude.com.br>
2022-06-29 03:27:45 -0300

    select screen rn

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import MeauButton from './lib/components/button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {
+      // Try setting `flexDirection` to `"row"`.
+      flexDirection: "column"
+    }]}>
+      <View style={styles.topTitle}>
+        <Text style={styles.text}>Olá</Text>
+      </View>
+      <View style={styles.buttonMenu}>
+        <MeauButton title='ADOTAR'></MeauButton>
+        <MeauButton title='AJUDAR'></MeauButton>
+        <MeauButton title='CADASTRAR ANIMAL'></MeauButton>
+        <Button title='login'></Button>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+  },
+  text: {
+    fontSize: 76,
+  },
+  topTitle: {
+    marginTop: 60,
+    height: 100,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonMenu: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   },
 });
