3f6f1d16f6193370f9972566e0ccf81ee30437a9
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-07-05 22:55:33 -0300

    Refactor code and add the firebase connection file
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { async } from "@firebase/util";
-import { View } from "react-native-web";
 
-import firebase from "./src/firebaseConnection";
-import Adotar from "./src/pages/Adotar";
-import Ajudar from "./src/pages/Ajudar";
-import CadastrarAnimal from "./src/pages/CadastrarAnimal";
+import Help from "./src/pages/Help";
 import Home from "./src/pages/Home";
+import Adopt from "./src/pages/Adopt";
 import Login from "./src/pages/Login";
+import RegisterAnimal from "./src/pages/CadastrarAnimal";
 import CadastrarUsuario from "./src/pages/CadastrarUsuario";
 
 const Stack = createNativeStackNavigator();
@@ -19,11 +16,11 @@ export default function App(){
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name = "Home" component={Home} />
-        <Stack.Screen name = "Adotar" component={Adotar} />
-        <Stack.Screen name = "Ajudar" component={Ajudar} />
-        <Stack.Screen name = "CadastrarAnimal" component={CadastrarAnimal} />
-        <Stack.Screen name = "CadastrarUsuario" component={CadastrarUsuario} />
+        <Stack.Screen name = "Adotar" component={Adopt} />
+        <Stack.Screen name = "Ajudar" component={Help} />
         <Stack.Screen name = "Login" component={Login} />
+        <Stack.Screen name = "CadastrarUsuario" component={CadastrarUsuario} />
+        <Stack.Screen name = "CadastrarAnimal" component={RegisterAnimal} />
       </Stack.Navigator>
     </NavigationContainer>
   )
