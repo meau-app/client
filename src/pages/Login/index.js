055b60312a63751aa896689754de0f97287d805e
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-07-11 03:28:37 -0300

    Fix login page
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,34 @@
-import {useState} from "react";
-import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import { useState } from "react";
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 
 import { auth } from "../../service/database/firebase";
-import { TextInput } from "react-native-web";
 
 export default function Login() {
   const navigation = useNavigation();
-
-  function navigate(page) {
-    navigation.navigate(page);
-  }
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   function login() {
     auth.signInWithEmailAndPassword(email, password)
-        .then((credential) => {
-            console.log("credential is " + credential + " for user " + credential.user);
+        .then(credential => {
+            const user = credential.user.email;
+            const token = credential.user.getIdToken(true);
+            /*auth.currentUser.getIdToken(true)
+                .then(token => {
+
+                })*/
+
+            alert("credential is " + token + " for user " + user);
+
+
         })
         .catch(error => {
             const code = error.code;
             const message = error.message;
 
-            console.log("login failed with code " + code + " and reason " + message);
+            alert("login failed with code " + code + " and reason " + message);
         });
   }
 
@@ -32,19 +36,23 @@ export default function Login() {
     <View style={styles.container}>
       <Text>Email</Text>
       <TextInput
+        style={styles.input}
         onChangeText={setEmail}
         value={email}
       />
 
       <Text>Senha</Text>
       <TextInput
+        style={styles.input}
         onChangeText={setPassword}
         value={password}
       />
-      <TouchableOpacity style={styles.button} onPress={()=>login()}>
-        Login
-      </TouchableOpacity>
-      <Button title="Home" onPress={()=>navigate('Home')}/>
+
+      <Button
+        style={styles.button}
+        title="Login"
+        onPress={login}
+      />
     </View>
   )
 }
@@ -52,11 +60,19 @@ export default function Login() {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    margin: 10,
   },
   button: {
     alignItems: "center",
     padding:10
+  },
+  input:{
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    height: 45,
+    fontSize: 17
   }
 })
