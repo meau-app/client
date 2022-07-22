722201f3f0ac2dbf97cb4f19db4e8463f69a380a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-07-22 18:15:25 -0300

    Improve routes descriptions and models
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- App.tsx
+++ App.tsx
@@ -1,28 +1,51 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  DefaultTheme as theme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 
-import Help from "./src/pages/Help";
-import Preload from "./src/pages/Preload";
-import Home from "./src/pages/Home";
-import Adopt from "./src/pages/Adopt";
-import Login from "./src/pages/Login";
-import RegisterAnimal from "./src/pages/RegisterAnimal";
-import RegisterUser from "./src/pages/RegisterUser";
+import Help from './src/pages/Help';
+import Preload from './src/pages/Preload';
+import Home from './src/pages/Home';
+import Adopt from './src/pages/Adopt';
+import Login from './src/pages/Login';
+import RegisterAnimal from './src/pages/RegisterAnimal';
+import RegisterUser from './src/pages/RegisterUser';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Preload" component={Preload} options={{headerShown: false}} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Adopt" component={Adopt} />
-        <Stack.Screen name="Help" component={Help} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="RegisterUser" component={RegisterUser} />
-        <Stack.Screen name="RegisterAnimal" component={RegisterAnimal} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Preload"
+            component={Preload}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Adopt" component={Adopt} />
+          <Stack.Screen name="Help" component={Help} />
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerTitle: 'Entrar' }}
+          />
+          <Stack.Screen
+            name="RegisterUser"
+            component={RegisterUser}
+            options={{ headerTitle: 'Registrar Usuário' }}
+          />
+          <Stack.Screen name="RegisterAnimal" component={RegisterAnimal} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
 }
