0033004c71bdf535710fc750d17b0b71298bf4ca
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-10 03:29:42 -0300

    Add a tab navigation to the app
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ src/pages/Notification/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Text } from 'react-native-paper';
+
+export default function Notification() {
+  return (
+    <ScrollView>
+      <Text>Notificações</Text>
+    </ScrollView>
+  );
+}
