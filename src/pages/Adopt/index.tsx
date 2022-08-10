341fa4965a134fcc4bfbc0483da6dc36b3d10828
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-10 20:01:50 -0300

    Update component of adopted animal
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/pages/Adopt/index.tsx
+++ src/pages/Adopt/index.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Alert, ScrollView } from 'react-native';
+import { Appbar, Avatar, Button, Card, IconButton, Text, } from 'react-native-paper';
+import { SafeAreaView, } from 'react-native-safe-area-context';
 
 import { Interface } from '../../service/api/interface';
 import { Pet } from '../../service/api/models/pet';
@@ -9,9 +11,18 @@ import styles from './styles'
 
 export default function Adopt() {
   let api = new Interface();
+    let navigation = useNavigation();
 
   const [state, setState] = useState('loading')
   const [pets, setPets] = useState(Array<Pet>);
+
+  function to(page: string): void {
+    let m = {
+      name: page,
+      key: '',
+    };
+    navigation.navigate(m);
+  }
 
   function request() {
     api.get(new Pet()).then(v => {
@@ -24,15 +35,29 @@ export default function Adopt() {
 
   return (
     request(),
-    <View style={styles.container}>
-        {state === 'loading' ? (
-            <Text>Carregando...</Text>
-        ) : (
-            pets.map((p, i) => {
-                return <Text key={i}>{p.name}</Text>
-            })
-        )}
-    </View>
+    <SafeAreaView>
+    <Appbar.Header style={styles.bar}>
+        <Appbar.Content title="ADOTE" titleStyle={styles.title}/>
+        <Appbar.Action icon="plus" style={styles.bar} onPress={() => {to('RegisterAnimal')}} />
+    </Appbar.Header>
+    <ScrollView>
+    <View>
+            {state === 'loading' ? (
+                <Text>Carregando...</Text>
+            ) : (
+                pets.map((p, i) => {
+                    return <Card onPress={() =>{to('')}}>
+                        <Card.Title
+                            title={p.name}
+                            subtitle={p.temper}
+                        />
+                        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+                  </Card>
+                })
+            )}
+        </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
