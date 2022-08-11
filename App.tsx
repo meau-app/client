4ac46bba7928b325cf0a85130ddc47c24d65718c
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-10 23:43:11 -0300

    Remove non defined pages
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- App.tsx
+++ App.tsx
@@ -3,14 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-import Help from './src/pages/Help';
 import Preload from './src/pages/Preload';
 import Home from './src/pages/Home';
-import Adopt from './src/pages/Adopt';
 import Login from './src/pages/Login';
 import RegisterAnimal from './src/pages/RegisterAnimal';
 import RegisterUser from './src/pages/RegisterUser';
-import Profile from './src/pages/Profile';
 
 const Stack = createNativeStackNavigator();
 const Theme = {
