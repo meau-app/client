43a75ee7743d5750027c368393bb692caf29731e
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-10 23:43:52 -0300

    Fix navigation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { ScrollView, Alert, View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
-import styles from './styles';
-import * as SecureStore from 'expo-secure-store';
-import Authentication from '../../service/authentication/authenticate';
-import { linkWithRedirect } from 'firebase/auth';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Adopt from '../Adopt';
 import Profile from '../Profile';
 import Notification from '../Notification';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 const Tab = createBottomTabNavigator();
 
-export default function Home() {
-  const navigation = useNavigation();
-
+export default function Home({ navigation }) {
   function to(page: string): void {
     let m = {
       name: page,
@@ -29,36 +20,32 @@ export default function Home() {
   }
 
   return (
-    <NavigationContainer independent={true}>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let icon;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let icon;
 
-            if (route.name === 'Adotar') {
-              icon = focused ? 'bug' : 'bug-outline';
-            } else if (route.name === 'Notificações') {
-              icon = focused
-                ? 'ios-notifications'
-                : 'ios-notifications-outline';
-            } else if (route.name === 'Perfil') {
-              icon = focused ? 'ios-person' : 'ios-person-outline';
-            }
+          if (route.name === 'Adotar') {
+            icon = focused ? 'bug' : 'bug-outline';
+          } else if (route.name === 'Notificações') {
+            icon = focused ? 'ios-notifications' : 'ios-notifications-outline';
+          } else if (route.name === 'Perfil') {
+            icon = focused ? 'ios-person' : 'ios-person-outline';
+          }
 
-            return <Ionicons name={icon} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#365047',
-          tabBarInactiveTintColor: '#BBBBB',
-        })}
-      >
-        <Tab.Screen
-          name="Adotar"
-          component={Adopt}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen name="Notificações" component={Notification} />
-        <Tab.Screen name="Perfil" component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
+          return <Ionicons name={icon} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#365047',
+        tabBarInactiveTintColor: '#BBBBB',
+      })}
+    >
+      <Tab.Screen
+        name="Adotar"
+        component={Adopt}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen name="Notificações" component={Notification} />
+      <Tab.Screen name="Perfil" component={Profile} />
+    </Tab.Navigator>
   );
 }
