bbd2f99373ea2ff281a85bf4ca8b8d8976831553
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-16 15:54:21 -0300

    Update adopt page
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/pages/Adopt/index.tsx
+++ src/pages/Adopt/index.tsx
@@ -1,18 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { View, Alert, ScrollView } from 'react-native';
-import { Appbar, Avatar, Button, Card, IconButton, Text, } from 'react-native-paper';
-import { SafeAreaView, } from 'react-native-safe-area-context';
+import {
+  Appbar,
+  Avatar,
+  Button,
+  Card,
+  IconButton,
+  Text,
+} from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Interface } from '../../service/api/interface';
 import { Pet } from '../../service/api/models/pet';
 
-import styles from './styles'
+import styles from './styles';
 
 export default function Adopt({ navigation }) {
   let api = new Interface();
 
-  const [state, setState] = useState('loading')
+  const [state, setState] = useState('loading');
   const [pets, setPets] = useState(Array<Pet>);
 
   function to(page: string): void {
@@ -24,42 +31,57 @@ export default function Adopt({ navigation }) {
   }
 
   function request() {
-    api.get(new Pet()).then(v => {
+    api
+      .get(new Pet())
+      .then(v => {
         setPets(v as Array<Pet>);
         setState('');
-    }).catch(e => {
+      })
+      .catch(e => {
         Alert.alert(e);
-    })
+      });
   }
 
   return (
     request(),
-    <SafeAreaView>
-    <Appbar.Header style={styles.bar}>
-        <Appbar.Content title="MEAU" titleStyle={styles.title}/>
-        <Appbar.Action icon="plus" style={styles.bar} onPress={() => {to('RegisterAnimal')}} />
-    </Appbar.Header>
-    <ScrollView>
-    <View>
+    (
+      <SafeAreaView>
+        <Appbar.Header style={styles.bar}>
+          <Appbar.Content title="MEAU" titleStyle={styles.title} />
+          <Appbar.Action
+            icon="plus"
+            style={styles.bar}
+            onPress={() => {
+              to('RegisterAnimal');
+            }}
+          />
+        </Appbar.Header>
+        <ScrollView>
+          <View>
             {state === 'loading' ? (
-                <Text>Carregando...</Text>
+              <Text>Carregando...</Text>
             ) : (
-                pets.map((p, i) => {
-                    return <Card key={i} onPress={() =>{to('')}}>
-                        <Card.Title
-                            title={p.name}
-                            subtitle={p.temper}
-                        />
-                        <Card.Cover source={{
-                            uri: 'https://picsum.photos/700'
-                        }} />
+              pets.map((p, i) => {
+                return (
+                  <Card
+                    key={i}
+                    onPress={() => {
+                      to('');
+                    }}
+                  >
+                    <Card.Title title={p.name} subtitle={p.temper} />
+                    <Card.Cover
+                      source={{
+                        uri: 'https://picsum.photos/700',
+                      }}
+                    />
                   </Card>
-                })
+                );
+              })
             )}
-        </View>
-    </ScrollView>
-    </SafeAreaView>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    )
   );
 }
-
-
