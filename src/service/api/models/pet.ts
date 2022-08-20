a44fec8fff0c75d95c1ac374185c773b55321fb5
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-20 16:23:24 -0300

    Add a response object interface
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/service/api/models/pet.ts
+++ src/service/api/models/pet.ts
@@ -2,13 +2,28 @@ import { Entity } from './entity';
 
 export type Sex = 'male' | 'female';
 
+/**
+ * PetResponse is a representation of response that has been sent by the
+ * backend.
+ */
+export interface PetResponse {
+  name: string;
+  temper: string;
+  adopted: boolean;
+  race: string;
+  sex: Sex;
+  age: number;
+  vaccines: Array<String>;
+  pictures: Array<String>;
+}
+
 export class Pet extends Entity {
   public properties = {
     id: '',
     temper: '',
     adopted: Boolean(),
     name: '',
-    species: '',
+    race: '',
     sex: '',
     age: 0,
     vaccines: Array<String>(),
@@ -30,10 +45,25 @@ export class Pet extends Entity {
     this.properties.adopted = adopted !== undefined ? adopted : false;
     this.properties.name = name !== undefined ? name : '';
     this.properties.temper = temper !== undefined ? temper : '';
-    this.properties.species = race !== undefined ? race : '';
+    this.properties.race = race !== undefined ? race : '';
     this.properties.age = age !== undefined ? age : 0;
     this.properties.sex = sex !== undefined ? sex : 'male';
     this.properties.vaccines = vaccines !== undefined ? vaccines : [];
     this.properties.pictures = pictures !== undefined ? pictures : [];
+  }
+
+  static build(response: PetResponse): Pet {
+    let p = new Pet();
+
+    p.properties.name = response.name;
+    p.properties.age = response.age;
+    p.properties.sex = response.sex;
+    p.properties.temper = response.temper;
+    p.properties.race = response.race;
+    p.properties.pictures = response.pictures;
+    p.properties.vaccines = response.vaccines;
+    p.properties.adopted = response.adopted;
+
+    return p;
   }
 }
