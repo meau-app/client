5440e7858d5680e7bec90f5c6abc7fa6855e8bd1
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-08-25 23:07:26 -0300

    Add guarded routes logic to the app
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
@@ -9,6 +9,7 @@ import Login from './src/pages/Login';
 import RegisterAnimal from './src/pages/RegisterAnimal';
 import RegisterUser from './src/pages/RegisterUser';
 import PetInfo from './src/pages/PetInfo';
+import Authentication from './src/service/authentication/authenticate';
 
 const Stack = createNativeStackNavigator();
 const Theme = {
@@ -24,45 +25,66 @@ const Theme = {
 };
 
 export default function App() {
+  const [signed, setSigned] = useState(false);
+
+  async function check() {
+    try {
+      let state = await Authentication.check();
+      setSigned(state);
+    } catch (e) {
+      // ignore
+    }
+  }
+
+  useEffect(() => {
+    check();
+  }, []);
+
   return (
     <PaperProvider theme={Theme}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Preload"
-            component={Preload}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{ headerTitle: 'Entrar' }}
-          />
-          <Stack.Screen
-            name="RegisterUser"
-            component={RegisterUser}
-            options={{ headerTitle: 'Registrar Usuário' }}
-          />
-          <Stack.Screen
-            name="RegisterAnimal"
-            component={RegisterAnimal}
-            options={{
-              headerTitle: 'Registrar Pet',
-            }}
-          />
-          <Stack.Screen
-            name="PetInfo"
-            component={PetInfo}
-            options={{
-              headerTitle: 'PetInfo',
-            }}
-          />
-        </Stack.Navigator>
+        {signed ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="RegisterAnimal"
+              component={RegisterAnimal}
+              options={{
+                headerTitle: 'Registrar Pet',
+              }}
+            />
+            <Stack.Screen
+              name="PetInfo"
+              component={PetInfo}
+              options={{
+                headerTitle: 'PetInfo',
+              }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Preload"
+              component={Preload}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerTitle: 'Entrar' }}
+            />
+            <Stack.Screen
+              name="RegisterUser"
+              component={RegisterUser}
+              options={{ headerTitle: 'Registrar Usuário' }}
+            />
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
     </PaperProvider>
   );
