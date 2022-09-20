40628dd671562c10770935271920ce02b757b563
Rafael Campos Nunes <rafaelnunes@engineer.com>
2022-09-20 20:53:47 -0300

    Add login verification
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- Context.tsx
+++ Context.tsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Authentication from './src/service/authentication/authenticate';
 
 const AuthContext = React.createContext();
 
-export const AuthProvider: React.FC = ({children}) => {
-    const [signed, setSigned] = useState(false);
+export const AuthProvider: React.FC = ({ children }) => {
+  const [signed, setSigned] = useState(false);
 
-    return (
-        <AuthContext.Provider value={{signed, setSigned}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  async function initialize() {
+    try {
+      let result = await Authentication.check();
+      if (result) {
+        setSigned(true);
+      }
+    } catch (e) {
+      // ignore
+    }
+  }
+
+  useEffect(initialize, []);
+
+  return (
+    <AuthContext.Provider value={{ signed, setSigned }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export const useAuth = () => React.useContext(AuthContext)
+export const useAuth = () => React.useContext(AuthContext);
